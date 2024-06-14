8324294f086e445074b099d5eee7e8d24d6f9d14
oddsjerome <naronkrach@odds.team>
2024-06-14 08:58:35 +0700

    Apply Tailwind

--- src/RandomCharacterGenerator.js
+++ src/RandomCharacterGenerator.js
@@ -15,10 +15,16 @@ const RandomCharacterGenerator = () => {
   }, []);
 
   return (
-    <div className="container">
-      <div className="character-box">
-        <h1 className="character">{character}</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-50">
+      <div className="flex items-center justify-center w-48 h-48 mb-8 text-6xl font-bold text-green-500 border-4 border-green-500 rounded-lg bg-white">
+        {character}
       </div>
+      <button 
+        className="px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600"
+        onClick={generateRandomCharacter}
+      >
+        Generate Character
+      </button>
     </div>
   );
 };
