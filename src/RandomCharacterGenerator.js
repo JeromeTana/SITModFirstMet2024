ce1779de179c4d75c9c441128d6246dae08c850e
Lostjerome <naronkrach@gmail.com>
2024-07-17 14:32:51 +0700

    Refactor

--- src/RandomCharacterGenerator.js
+++ src/RandomCharacterGenerator.js
@@ -31,21 +31,14 @@ const RandomCharacterGenerator = () => {
 
     return (
         <div className="flex items-center justify-center h-screen">
-            <div className="max-w-sm overflow-hidden rounded">
+            <div className="max-w-sm">
                 <div className="flip-card">
                     <div
                         className={`flip-card-inner ${
                             flipped ? "flipped" : ""
                         }`}
                     >
-                        <div className="bg-white flip-card-front">
-                            <div className="flex items-center justify-center h-full">
-                                <span className="text-[3rem] font-bold">
-                                    แรกพบ SIT น้อย
-                                </span>
-                            </div>
-                        </div>
-
+                        <div className="bg-white border-white border-[20px] flip-card-front" />
                         <div
                             className={`flip-card-back ${
                                 selectedChar === "?"
