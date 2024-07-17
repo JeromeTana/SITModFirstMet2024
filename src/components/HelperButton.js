3477b29d97018a46f7642d71e9eca140ff97f1cb
Lostjerome <naronkrach@gmail.com>
2024-07-17 13:59:32 +0700

    Refactor helper modal

--- src/components/HelperButton.js
+++ src/components/HelperButton.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import RefreshIcon from "./icons/RefreshIcon";
+import HelperModal from "./HelperModal";
 
 const HelperButton = () => {
     const [showHelperModal, setShowHelper] = useState(false);
@@ -15,36 +15,7 @@ const HelperButton = () => {
                 ?
             </button>
             {showHelperModal && (
-                <div
-                    className="fixed inset-0 flex items-center justify-center text-xl leading-9 bg-black bg-opacity-50"
-                    onClick={toggleHelperModal}
-                >
-                    <div className="w-full p-8 m-5 bg-white rounded-3xl">
-                        <h2 className="mb-5 text-2xl font-bold">วิธีการเล่น</h2>
-                        <ul className="list-disc list-inside">
-                            <li>
-                                <p>
-                                    ทุก ๆ ครั้ง จะสุ่มตัวอักษรภาษาอังกฤษ
-                                    <span> A-Z </span>ขึ้นมาแสดง 1 ตัว
-                                </p>
-                            </li>
-                            <li>
-                                <p className="inline-flex">
-                                    กด
-                                    <RefreshIcon />
-                                    เพื่อสุ่มตัวอักษรใหม่
-                                </p>
-                            </li>
-                            <li>
-                                <p>
-                                    <span className="px-2 py-1 font-bold rounded-lg bg-gradient-to-tr from-pink-400 to-orange-300">
-                                        Special card
-                                    </span> จะแสดงเครื่องหมาย <span className="font-bold">?</span> และสามารถพิมพ์ตัวอักษรอะไรก็ได้เข้าไป 1 ตัว
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <HelperModal toggleHelperModal={toggleHelperModal} />
             )}
         </div>
     );
