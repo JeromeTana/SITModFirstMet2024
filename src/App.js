22bcf77cc27e552388d2a087c586ad54b3b55a8b
Lostjerome <naronkrach@gmail.com>
2024-07-18 17:52:04 +0700

    Add analytics

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React from 'react';
 import './styles/App.css';
 import RandomCharacterGenerator from './RandomCharacterGenerator';
 import FallingObjectBackground from './FallingObjectBackground';
+import { Analytics } from "@vercel/analytics/react";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <body>
         <FallingObjectBackground image={'/images/ant.png'}>
           <RandomCharacterGenerator />
         </FallingObjectBackground>
+        <Analytics/>
+        </body>
       </header>
     </div>
   );
